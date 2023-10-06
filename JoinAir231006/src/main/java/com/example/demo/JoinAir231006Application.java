34b8904d26284044649ac34580b346c189e74b7c
Leodohyun <prada5144@gmail.com>
2023-10-06 11:50:07 +0900

    new project

---
+++ JoinAir231006/src/main/java/com/example/demo/JoinAir231006Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinAir231006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinAir231006Application.class, args);
+	}
+
+}
---
+++ JoinAir231006/src/test/java/com/example/demo/JoinAir231006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinAir231006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
